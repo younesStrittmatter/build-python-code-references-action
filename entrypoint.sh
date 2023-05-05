ec7bed8944ba15d5631ed81ab771024b6962011d
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-05-05 08:02:32 +0000

    bug: paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-cd $GITHUB_WORKSPACE
+# Get the absolute path to the generate_readme.py script
+generate_readme_script_path="/generate_readme.py"
 
-python generate_readme.py $1 $2
+# Get the full paths for the input and output files
+input_file="$GITHUB_WORKSPACE/$1"
+output_file="$GITHUB_WORKSPACE/$2"
+
+# Execute the generate_readme.py script with the provided arguments
+python $generate_readme_script_path $input_file $output_file
